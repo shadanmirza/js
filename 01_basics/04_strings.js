73021eb40b8537192bad92ef6d7f1dac66d5b535
shadanmirza <shadanmirzanew@gmail.com>
2026-07-15 12:02:48 +0000

    math_function

--- 01_basics/04_strings.js
+++ 01_basics/04_strings.js
@@ -33,3 +33,52 @@ console.log(url.replace('%20', '-'))
 console.log(url.includes('sundar'))
 
 console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+// self ................................................................................................................................................................................................................................................
+let name = "shad"
+let age = "20"
+// console.log(`Hello my name is ${name} and my repo count is ${age}`);
+
+const gameName = new String("shadan")
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+//  console.log(gameName.big()); //
+
+
+
+// use of anchor in string ..........
+const stringOne = gameName.anchor("hello"); // use of anchor in string
+// console.log(stringOne);
+
+
+
+
+
+// use of at() in string ...........
+ const sentence = "i am  captain zura";
+
+ let index = 3;
+// console.log(`An index of  returns the character ${sentence.at(3)}`);
+
+
+//  index = -6;
+// console.log(`An index of ${index} returns the character ${sentence.at(index)}`);
+
+
+
+
+// use of Charat() and charCodeat() in string ...........
+let story = "once upon a time";
+
+// console.log(`the 5th character is ${story.charAt(5)} is equal to ${story.charCodeAt(5)}`);
---
+++ 01_basics/05_num_math.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4)); // to fix the value after the point
+
+const otherNumber = 200.8966
+
+// console.log(otherNumber.toPrecision(3));  // to round off the value
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4)); // to turn the value in positive
+
+// console.log(Math.round(4.6)); // to round off the value
+
+// console.log(Math.ceil(4.2)); // upper round off the value if only 0.1 extra in the value 
+
+// console.log(Math.floor(4.9)); // lower round off the value if only 0.1 less in the value
+
+// console.log(Math.min(4, 3, 6, 8)); // to find minimun num
+
+// console.log(Math.max(4, 3, 6, 8)); //  to find minimun num
+
+// console.log(Math.random()); // it gives the random value in between 0 to 1
+
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+//  console.log(Math.floor(Math.random() * (max - min + 1)) + min) // formula to fix range
